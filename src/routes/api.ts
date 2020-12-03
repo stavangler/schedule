6d78537eb2944610fb5636a9bbbd401ea241aebb
nniclas <niclas.oes@gmail.com>
2020-12-03 19:50:51 +0100

    minor changes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,50 +1,26 @@
 import * as express from 'express';
-import pgPromise from 'pg-promise';
-import QueryDay from '../models/query-day';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
 export const register = (app: express.Application) => {
-    const port = parseInt(process.env.DB_PORT || '5432', 10);
-    const config = {
-        database: process.env.DB_DATABASE || 'pgdb',
-        host: process.env.DB_HOST || 'localhost',
-        port,
-        user: process.env.DB_USER || 'user',
-        password: process.env.DB_PASSWORD || 'password',
-    };
-
-    const pgp = pgPromise();
-    const db = pgp(config);
 
     app.get('/api/trip/day', async (req: any, res) => {
 
         // req.params.queryDay
-
         try {
-
-            let main = async () => {
-                const result = await prisma.tags.findMany();
+            const main = async () => {
+                const result = await prisma.entries.findMany();
                 console.log(result);
             }
 
             main()
                 .catch(e => {
-                    throw e
+                    throw e;
                 })
                 .finally(async () => {
-                    await prisma.$disconnect()
+                    await prisma.$disconnect();
                 })
-
-            //   const date = '2018-04-12';
-            //   const day = await db.any(`
-            //             SELECT id, title
-            //             FROM days
-            //             WHERE date = $[date]
-            //             ORDER BY starttime ASC`, { date });
-            //   // ...
-            // return res.json(day);
 
         } catch (err) {
             // tslint:disable-next-line:no-console
