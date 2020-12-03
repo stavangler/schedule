647a4ee4b2aca69cd03fc665d81b21b41fb380b2
nniclas <niclas.oes@gmail.com>
2020-12-03 14:30:32 +0100

    added prisma orm

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,39 +1,55 @@
 import * as express from 'express';
 import pgPromise from 'pg-promise';
-import QueryDay from '../models/query-day'
+import QueryDay from '../models/query-day';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 export const register = (app: express.Application) => {
-  const port = parseInt(process.env.DB_PORT || '5432', 10);
-  const config = {
-    database: process.env.DB_DATABASE || 'pgdb',
-    host: process.env.DB_HOST || 'localhost',
-    port,
-    user: process.env.DB_USER || 'user',
-    password: process.env.DB_PASSWORD || 'password',
-  };
+    const port = parseInt(process.env.DB_PORT || '5432', 10);
+    const config = {
+        database: process.env.DB_DATABASE || 'pgdb',
+        host: process.env.DB_HOST || 'localhost',
+        port,
+        user: process.env.DB_USER || 'user',
+        password: process.env.DB_PASSWORD || 'password',
+    };
 
-  const pgp = pgPromise();
-  const db = pgp(config);
+    const pgp = pgPromise();
+    const db = pgp(config);
 
-  app.get('/api/trip/day', async (req: any, res) => {
+    app.get('/api/trip/day', async (req: any, res) => {
 
-    // req.params.queryDay
+        // req.params.queryDay
 
-    try {
-      const date = '2018-04-12';
-      const day = await db.any(`
-                SELECT id, title
-                FROM days
-                WHERE date = $[date]
-                ORDER BY starttime ASC`, { date });
+        try {
 
-      // ...
+            let main = async () => {
+                const result = await prisma.tags.findMany();
+                console.log(result);
+            }
 
-      return res.json(day);
-    } catch (err) {
-      // tslint:disable-next-line:no-console
-      console.error(err); // eslint-disable-line no-console
-      res.json({ error: err.message || err });
-    }
-  });
+            main()
+                .catch(e => {
+                    throw e
+                })
+                .finally(async () => {
+                    await prisma.$disconnect()
+                })
+
+            //   const date = '2018-04-12';
+            //   const day = await db.any(`
+            //             SELECT id, title
+            //             FROM days
+            //             WHERE date = $[date]
+            //             ORDER BY starttime ASC`, { date });
+            //   // ...
+            // return res.json(day);
+
+        } catch (err) {
+            // tslint:disable-next-line:no-console
+            console.error(err); // eslint-disable-line no-console
+            res.json({ error: err.message || err });
+        }
+    });
 };
